e9d88d8dd2a8c44a025ec0aa5c5fdc1b2d222077
Soumil Singh <soumil.sps@gmail.com>
2025-10-18 22:02:10 +0530

    base layout

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <h1>I mean good for now ig.....</h1>
+        </>
+    )
+}
---
+++ config/site.ts
@@ -0,0 +1,57 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Portfolio",
+  description: "Hey, I'm Soumil, Welcome",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "Let's Talk",
+      href: "/contact",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    }
+  ],
+  links: {
+    github: "https://github.com/heroui-inc/heroui",
+    twitter: "https://twitter.com/hero_ui",
+    docs: "https://heroui.com",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
