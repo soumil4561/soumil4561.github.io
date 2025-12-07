287b4155ab52fdf95498e5c0f7a3f52925e75251
Soumil Singh <soumil.sps@gmail.com>
2025-12-07 22:00:48 +0530

    updating layout

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import About from "./about";
+
+export default function AboutPage() {
+  return (
+    <section className="section pt-0">
+      <About />
+    </section>
+  );
+}
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -30,22 +30,16 @@ export default async function BlogPost({
   const mdxSource = await serialize({ source: post.content });
 
   return (
-    <div className="relative flex flex-col min-h-screen">
-      <div className="h-16" />
-      <main className="container mx-auto max-w-8xl px-6 flex-grow">
-        <section className="section">
-          <div className="page-heading">{post.frontmatter.title}</div>
-          <div className="page-heading-subtitle">
-            {post.frontmatter.description}
-          </div>
-          {/* read more button */}
-          {/* then the three bars: date, tags, reading time */}
-          {/* picture */}
-          {/* then the mdx parse */}
-          <MDXRenderer mdxSource={mdxSource} />
-        </section>
-      </main>
-      <footer className="w-full flex items-center justify-center py-3" />
-    </div>
+    <section className="section">
+      <div className="page-heading">{post.frontmatter.title}</div>
+      <div className="page-heading-subtitle">
+        {post.frontmatter.description}
+      </div>
+      {/* read more button */}
+      {/* then the three bars: date, tags, reading time */}
+      {/* picture */}
+      {/* then the mdx parse */}
+      <MDXRenderer mdxSource={mdxSource} />
+    </section>
   );
 }
